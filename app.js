e5e5a7edfa5a197ff2d9bac729abe26fca44697f
Mercy-Techy <oluwagbemiro65@gmail.com>
2023-03-01 11:57:19 +0100

    wanted to check something

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(multer({storage:fileStorage,fileFilter:fileFilter}).single('image'))
 
 
 app.use((req,res,next)=>{
-  res.setHeader('Access-Control-Allow-Origin','*')
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods",'GET,POST,PUT,PATCH,DELETE');
   res.setHeader("Access-Control-Allow-Headers",'Content-Type,Authorization');
   next()
